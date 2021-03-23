1083574f078827e99efd6754021beddcbe4bed71
Orozco Camacho Luis Miguel <80171936+luismi999@users.noreply.github.com>
2021-03-23 10:26:50 -0600

    Validación de categoría V1

--- js/funciones.js
+++ js/funciones.js
@@ -3,10 +3,11 @@ function validarFormVacio(formulario){
 		d=datos.split('&');
 		vacios=0;
 		for(i=0;i< d.length;i++){
-				controles=d[i].split("=");
-				if(controles[1]=="A" || controles[1]==""){
-					vacios++;
-				}
+			controles=d[i].split("=");
+			var val = controles[1].indexOf("%20");
+			if(controles[1]=="A"||controles[1]==""||val!=-1){
+				vacios++;
+			}
 		}
 		return vacios;
-	}
+	}	
